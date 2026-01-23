1d7a457c331f1b39b6af4dcf414a0dcbeda9275e
Tayler Nguyen <tayler.nguyen3306@gmail.comgit config --global user.email tayler.nguyen3306@gmail.comgit config --global init.defaultBranch main>
2026-01-22 16:00:32 -0800

    gh pages usability'

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/berkeleylphie-website/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
